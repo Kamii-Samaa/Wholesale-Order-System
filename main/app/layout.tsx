af8f830978512e2e6aa95a6e6687d922939b147d
Kamii-Samaa <kristophardivine@gmail.com>
2025-06-14 02:01:51 +0100

    Revert "dev fix"
    This reverts commit 797f0807f71d281b5dd0856c5d8e9f103ece9ef5.

--- main/app/layout.tsx
+++ main/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Wholesale Order System',
-  description: 'Created by Kamii Samaaa',
-  generator: 'Kamii Samaaa',
+  title: 'v0 App',
+  description: 'Created with v0',
+  generator: 'v0.dev',
 }
 
 export default function RootLayout({
